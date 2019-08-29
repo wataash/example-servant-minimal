2fb12980587f668e9858d410a20d9280839a515e
Wataru Ashihara <wsh@iij.ad.jp>
2019-08-29 19:59:25 +0900

    [d]

--- src/App.hs
+++ src/App.hs
@@ -5,12 +5,12 @@
 
 module App where
 
-import           Data.Aeson
-import           GHC.Generics
+import           Data.Aeson (ToJSON, FromJSON)
+import           GHC.Generics (Generic)
 import           Network.Wai
-import           Network.Wai.Handler.Warp
-import           Servant
-import           System.IO
+import           Network.Wai.Handler.Warp (setPort, setBeforeMainLoop, defaultSettings, runSettings)
+import           Servant -- ((:>), Get, JSON, (:<|>), Capture, Proxy, Server, Handler, serve, throwError, err404)
+import           System.IO (hPutStrLn, stderr)
 
 -- * api
 
@@ -18,6 +18,7 @@ type ItemApi =
   "item" :> Get '[JSON] [Item] :<|>
   "item" :> Capture "itemId" Integer :> Get '[JSON] Item
 
+-- http://localhost:3000/item
 itemApi :: Proxy ItemApi
 itemApi = Proxy
 
@@ -35,19 +36,27 @@ run = do
 mkApp :: IO Application
 mkApp = return $ serve itemApi server
 
+-- [{"itemId":0,"itemText":"example item"}]
+-- itemApi ->
 server :: Server ItemApi
 server =
   getItems :<|>
   getItemById
 
+-- http://localhost:3000/item   [{"itemId":0,"itemText":"example item"}]
+-- itemApi -> server ->
 getItems :: Handler [Item]
-getItems = return [exampleItem]
+getItems = return [exampleItem] -- 
 
+-- http://localhost:3000/item/0  {"itemId":0,"itemText":"example item"}
+-- http://localhost:3000/item/1  404
+-- itemApi ->  server ->
 getItemById :: Integer -> Handler Item
 getItemById = \ case
   0 -> return exampleItem
   _ -> throwError err404
 
+-- itemApi -> server -> getItems -> 
 exampleItem :: Item
 exampleItem = Item 0 "example item"
 
